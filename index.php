8a7ace32891eeb21d14671857dfde5b092e0f718
sid77320 <cancel.cyril@yahoo.fr>
2015-12-02 16:57:32 +0100

    initial design

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>
+            Base page groupe
+        </title>
+
+        
+        <link rel="stylesheet" href="css/style.css"/>
+    </head>
+
+    <body>
+      
+    </body>
+</html>
